b08232d71d0cf9ff8d192d631c3844cd7e678d5a
Liam Vermette <Liam.vermette@gmail.com>
2024-05-01 18:59:43 -0400

    implemented delete helper method

--- controllers/bread-controller.js
+++ controllers/bread-controller.js
@@ -69,10 +69,13 @@ breads.put("/:arrayIndex", (req, res) => {
   res.redirect(`/breads/${req.params.arrayIndex}`);
 });
 
-// DELETE
-breads.delete("/:indexArray", (req, res) => {
-  Bread.splice(req.params.indexArray, 1);
-  res.status(303).redirect("/breads");
-});
+//delete
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
+})
+
 
 module.exports = breads;
