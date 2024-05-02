3c45ca39baf370c971734a7a8965a454fdd88668
Liam Vermette <Liam.vermette@gmail.com>
2024-05-01 21:09:30 -0400

    added custom helper method to find baker

--- views/show.jsx
+++ views/show.jsx
@@ -1,30 +1,31 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/default");
 
-function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
-        <Default>
-            <h3>{bread.name}</h3>
-            <p>
-                and it
-                {
-                    bread.hasGluten
-                    ? <span> does </span>
-                    : <span> does NOT </span>
-                }
-                have gluten.
-            </p>
-            <img src={bread.image} alt={bread.name} />
-            <p>Baked by {bread.baker}</p>
-            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
-            <form action={`/breads/${bread._id}?_method=DELETE`} method="POST">
-              <input type='submit' value="DELETE"/>
-            </form>
-            <li><a href="/breads">Go home</a></li>
-        </Default>
-      )
-  }
+function Show({ bread }) {
+  // Confirm we are getting our bread data in the terminal.
+  // console.log(bread.name)
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <p>{bread.getBakedBy()}</p>
+      <a href={`/breads/${bread.id}/edit`}>
+        <button>Edit</button>
+      </a>
 
-module.exports = Show
+      <form action={`/breads/${bread._id}?_method=DELETE`} method="POST">
+        <input type="submit" value="DELETE" />
+      </form>
+      <li>
+        <a href="/breads">Go home</a>
+      </li>
+    </Default>
+  );
+}
+
+module.exports = Show;
