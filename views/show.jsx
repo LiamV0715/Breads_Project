bf9212103caaca5776b568ebb506dc64b1b2adfb
Liam Vermette <Liam.vermette@gmail.com>
2024-05-01 21:09:47 -0400

    added custom helper method to find baker

--- views/show.jsx
+++ views/show.jsx
@@ -17,7 +17,6 @@ function Show({ bread }) {
       <a href={`/breads/${bread.id}/edit`}>
         <button>Edit</button>
       </a>
-
       <form action={`/breads/${bread._id}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>
